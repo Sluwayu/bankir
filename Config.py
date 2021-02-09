08c427d29a1cee0c65816799e3286fc0c33fbe93
Sluwayu <63704250+Sluwayu@users.noreply.github.com>
2021-02-09 23:10:08 +0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -24,11 +24,11 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
+        "**Force**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
         
-        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        "**Sep**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
         
-        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
+        "**Comm**\n__/Forcee - To get the current settings.\n/Foribe no/off/disable - To turn of ForceSubscribe.\n/Forib - To turn on and setup the channel.\n/Forbe  - To unmute all members who muted by me.\n\nNote: /cribe__",
         
         "**@omadlottouz Adminidan**"
       ]
